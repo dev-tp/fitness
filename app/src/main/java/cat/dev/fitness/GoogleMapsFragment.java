6745a1b10a6b2476448eb37d53c56299f754cedb
Eryk <rcksorto@yahoo.com>
2017-07-21 20:00:01 -0700

    Design a simple stopwatch layout
    The design integrates Google Maps

--- app/src/main/java/cat/dev/fitness/GoogleMapsFragment.java
+++ app/src/main/java/cat/dev/fitness/GoogleMapsFragment.java
@@ -6,12 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
 public class GoogleMapsFragment extends Fragment implements OnMapReadyCallback {
+
+    private boolean pause;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,5 +33,17 @@ public class GoogleMapsFragment extends Fragment implements OnMapReadyCallback {
 
         ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_view))
                 .getMapAsync(this);
+
+        final Button startPauseButton = (Button) getActivity().findViewById(R.id.start_pause_button);
+        final Button stopButton = (Button) getActivity().findViewById(R.id.stop_button);
+
+        startPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pause = !pause;
+                startPauseButton.setText(pause ? R.string.action_pause : R.string.action_start);
+                stopButton.setEnabled(pause);
+            }
+        });
     }
 }
